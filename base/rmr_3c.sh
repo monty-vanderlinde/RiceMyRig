897ddd1e90990537f0c163fd774d9d427eb536e0
monty-vanderlinde <monty.vanderlinde@protonmail.com>
2020-07-15 08:15:07 -0500

    Pre-test completion of rmr_3c.sh

--- base/rmr_3c.sh
+++ base/rmr_3c.sh
@@ -16,6 +16,7 @@ dev3="/dev/sdc"
 root_encr="/dev/mapper/root"
 home_raid="/dev/md/home"
 home_encr="/dev/mapper/home"
+mac_spoof=""
 public_ip=""
 static_ip="192.168.0.253"
 gateway_ip="192.168.0.1"
@@ -111,76 +112,80 @@ mkfs.ext4 -L ROOT "$root_encr" || error_exit "Failed mkfs.ext4 on $root_encr"
 mkfs.ext4 -m 0 -L HOME "$home_encr" || error_exit "Failed mkfs.ext4 on $home_encr"
 
 # Mount the file systems, except for /efi
-mount "$root_encr" /mnt
-mkdir /mnt/efi /mnt/home
-mount "${dev1}1" /mnt/efi
-mount "$home_encr" /mnt/home
+mount "$root_encr" /mnt || error_exit "Failed to mount $root_encr"
+mkdir /mnt/efi /mnt/home || error_exit "Mkdir failed for either efi or home directory"
+mount "${dev1}1" /mnt/efi || error_exit "Failed to mount ${dev1}1"
+mount "$home_encr" /mnt/home || error_exit "Failed to mount $home_encr"
 
 # Setup swap
-dd if=/dev/zero of=/mnt/swapfile bs=1MiB count="$ram_mib" status=progress
-chmod 600 /mnt/swapfile
-mkswap /mnt/swapfile
-swapon /mnt/swapfile
+dd if=/dev/zero of=/mnt/swapfile bs=1MiB count="$ram_mib" status=progress || error_exit "Failed to make swapfile"
+chmod 600 /mnt/swapfile || error_exit "Failed to set swapfile permissions"
+mkswap /mnt/swapfile || error_exit "Mkswap failed on swapfile"
+swapon /mnt/swapfile || error_exit "Failed to turn swap on"
 
 # Edit the mirrorlist
-reflector --country 'United States' --country 'Canada' --latest 5 --age 24 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --country 'United States' --country 'Canada' --latest 5 --age 24 --sort rate --save /etc/pacman.d/mirrorlist ||
+	error_exit "Failed to update mirrors with reflector"
 
 # Install essential packages - yes to all prompts
-pacstrap /mnt base linux linux-firmware neovim man-db man-pages git mdadm cryptsetup reflector sudo intel-ucode dash parted pacman-contrib grub efibootmgr
+pacstrap /mnt base linux linux-firmware neovim man-db man-pages git mdadm cryptsetup reflector sudo intel-ucode dash parted pacman-contrib grub efibootmgr iproute2 ||
+	error_exit "Failed to download packages with pacstrap"
 
 # Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab || error_exit "Failed to generate fstab"
 
 # Remove that annoying beeping!
-arch-chroot /mnt rmmod pcspkr
-printf "blacklist pcspkr" > /mnt/etc/modprobe.d/nobeep.conf
+arch-chroot /mnt rmmod pcspkr || error_exit "Failed rmmod for pcspkr"
+printf "blacklist pcspkr" > /mnt/etc/modprobe.d/nobeep.conf || error_exit "Failed to blacklist pcspkr"
 
 # Load us keyboard and swap escape and caps-lock keys
-arch-chroot /mnt loadkeys us
-printf "keycode 1 = Caps_Lock\nkeycode 58 = Escape" > /mnt/usr/share/kbd/keymaps/personal.map
-arch-chroot /mnt loadkeys /usr/share/kbd/keymaps/personal.map
-printf "KEYMAP=us\nKEYMAP=/usr/share/kbd/keymaps/personal.map" > /mnt/etc/vconsole.conf
+arch-chroot /mnt loadkeys us || error_exit "Failed to loadkeys for us"
+printf "keycode 1 = Caps_Lock\nkeycode 58 = Escape" > /mnt/usr/share/kbd/keymaps/personal.map || error_exit "Failed to define personal keymap"
+arch-chroot /mnt loadkeys /usr/share/kbd/keymaps/personal.map || error_exit "Failed to loadkeys for personal keymap"
+printf "KEYMAP=us\nKEYMAP=/usr/share/kbd/keymaps/personal.map" > /mnt/etc/vconsole.conf || error_exit "Failed to set vconsole keymaps"
 
 # Set the timezone
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime || error_exit "Failed to link zoneinfo to localtime"
 
 # Set the clock
-arch-chroot /mnt timedatectl set-ntp true
-arch-chroot /mnt timedatectl set-local-rtc 0
-arch-chroot /mnt hwclock --systohc
+arch-chroot /mnt timedatectl set-ntp true || error_exit "Failed to update clock"
+arch-chroot /mnt timedatectl set-local-rtc 0 || error_exit "Failed to setup localtime clock"
+arch-chroot /mnt hwclock --systohc || error_exit "Failed to update hardware clock"
 
 # Generate the locale
-mv /mnt/etc/locale.gen /mnt/etc/locale.gen.bak
-sed -e 's/#en_US\.UTF-8 UTF-8/en_US\.UTF-8 UTF-8/' /mnt/etc/locale.gen.bak > /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
+mv /mnt/etc/locale.gen /mnt/etc/locale.gen.bak || error_exit "Failed to backup locale.gen"
+sed -e 's/#en_US\.UTF-8 UTF-8/en_US\.UTF-8 UTF-8/' /mnt/etc/locale.gen.bak > /mnt/etc/locale.gen || error_exit "Failed to generate locale.gen file"
+arch-chroot /mnt locale-gen || error_exit "Failed to update locale-gen"
 
 # Set the language
-printf "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+printf "LANG=en_US.UTF-8" > /mnt/etc/locale.conf || error_exit "Failed to define locale.conf"
 
 # Set the hostname
-printf "$hostname" > /mnt/etc/hostname
+printf "$hostname" > /mnt/etc/hostname || error_exit "Failed to set hostname"
 
 # Update mandb
-arch-chroot /mnt mandb
+arch-chroot /mnt mandb || error_exit "Failed to update mandb"
 
 # Setup hosts file and define static ip address
 printf "%-16s%s\n" \
 	"::1" "localhost" \
 	"127.0.0.1" "localhost" \
 	"$static_ip" "homelocal $hostname" \
-	"$public_ip" "home" > /mnt/etc/hosts
+	"$public_ip" "home" > /mnt/etc/hosts || error_exit "Failed to define hosts"
 
 # Setup crypttab to open home containter
-cp /etc/crypttab /etc/crypttab.bak
-printf "home\tUUID=$(lsblk -lp -o NAME,UUID | grep "$home_raid" | awk '{print $2}')\tnone\tluks,timeout=60" >> /mnt/etc/crypttab
+cp /etc/crypttab /etc/crypttab.bak || error_exit "Failed to backup crypttab"
+printf "home\tUUID=$(lsblk -lp -o NAME,UUID | grep "$home_raid" | awk '{print $2}')\tnone\tluks,timeout=60" >> /mnt/etc/crypttab ||
+	error_exit "Failed to define crypttab"
 
 # Setup hooks for mkinitcpio
-mv /mnt/etc/mkinitcpio.conf /mnt/etc/mkinitcpio.conf.bak
-sed -e 's/^HOOKS=.*/HOOKS=\(base udev autodetect keyboard keymap modconf block mdadm_udev encrypt filesystems resume fsck\)/g' /mnt/etc/mkinitcpio.conf.bak > /mnt/etc/mkinitcpio.conf
-arch-chroot /mnt mkinitcpio -P
+mv /mnt/etc/mkinitcpio.conf /mnt/etc/mkinitcpio.conf.bak || error_exit "Failed to backup mkinitcpio"
+sed -e 's/^HOOKS=.*/HOOKS=\(base udev autodetect keyboard keymap modconf block mdadm_udev encrypt filesystems resume fsck\)/g' /mnt/etc/mkinitcpio.conf.bak > /mnt/etc/mkinitcpio.conf ||
+	error_exit "Failed to set mkinitcpio hooks"
+arch-chroot /mnt mkinitcpio -P || error_exit "Failed to update mkinitcpio"
 
 # Configure grub
-mv /mnt/etc/default/grub /mnt/etc/default/grub.bak
+mv /mnt/etc/default/grub /mnt/etc/default/grub.bak || error_exit "Failed to backup grub configuration"
 printf "%s\n" \
 	"GRUB_DEFAULT=saved" \
 	"GRUB_TIMEOUT=5" \
@@ -193,13 +198,13 @@ printf "%s\n" \
 	"GRUB_TERMINAL=console"
 	"GRUB_GFXMODE=auto"
 	"GRUB_GFXPAYLOAD_LINUX=keep"
-	"GRUB_DISABLE_RECOVERY=true" > /mnt/etc/default/grub
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=ArchLinux
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+	"GRUB_DISABLE_RECOVERY=true" > /mnt/etc/default/grub || error_exit "Failed to define grub configuration"
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=ArchLinux || error_exit "Failed to install grub"
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg || error_exit "Failed grub-mkconfig"
 
-mdadm --detail --scan >> /mnt/etc/mdadm.conf
+mdadm --detail --scan >> /mnt/etc/mdadm.conf || error_exit "Failed to define madam.conf"
 
-mkdir /mnt/etc/pacman.d/hooks
+mkdir /mnt/etc/pacman.d/hooks || error_exit "Mkdir failed for /etc/pacman.d/hooks"
 printf "%s\n" \
 	"[Trigger]" \
 	"Operation = Upgrade" \
@@ -210,18 +215,19 @@ printf "%s\n" \
 	"Description = Updating pacman-mirrorlist with reflector and removing pacnew" \
 	"When = PostTransaction" \
 	"Depends = reflector" \
-	"Exec = /bin/sh -c \"reflector --country 'United States' --country 'Canada' --latest 5 --age 24 --sort rate --save /etc/pacman.d/mirrorlist\"" > /etc/pacman.d/hooks/mirrorupgrade.hook
+	"Exec = /bin/sh -c \"reflector --country 'United States' --country 'Canada' --latest 5 --age 24 --sort rate --save /etc/pacman.d/mirrorlist\"" > /etc/pacman.d/hooks/mirrorupgrade.hook ||
+		error_exit "Failed to define pacman reflector hook"
 
-arch-chroot /mnt groupadd "${groupname:-$username}"
-arch-chroot /mnt useradd -m -g "${groupname:-$username}" -G wheel -s /bin/bash "$username"
-arch-chroot /mnt passwd "$username"
+arch-chroot /mnt groupadd "${groupname:-$username}" || error_exit "Failed to add group ${groupname:-$username}"
+arch-chroot /mnt useradd -m -g "${groupname:-$username}" -G wheel -s /bin/bash "$username" || error_exit "Failed to add user $username"
+arch-chroot /mnt passwd "$username" || error_exit "Failed to update ${username}'s password"
 
-mv /mnt/etc/sudoers /mnt/etc/sudoers.bak
-sed -e 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /mnt/etc/sudoers.bak > /mnt/etc/sudoers
+mv /mnt/etc/sudoers /mnt/etc/sudoers.bak || error_exit "Failed to backup sudoers file"
+sed -e 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /mnt/etc/sudoers.bak > /mnt/etc/sudoers || error_exit "Failed to define sudoers file"
 
-arch-chroot /mnt ln -sf /bin/dash /bin/sh
+arch-chroot /mnt ln -sf /bin/dash /bin/sh || error_exit "Failed to link dash to sh"
 
-printf "nameserver $gateway_ip" >> /mnt/etc/resolv.conf
+printf "nameserver $gateway_ip" >> /mnt/etc/resolv.conf || error_exit "Failed to define nameserver"
 printf "%s\n" \
 	"[Unit]" \
 	"Description = Turn on internet access" \
@@ -229,6 +235,7 @@ printf "%s\n" \
 	"[Service]" \
 	"Type = oneshot" \
 	"ExecStart = /usr/bin/sh -c 'ip addr add $static_ip/24 broadcast + dev $eth_interface'" \
+	"ExecStart = /usr/bin/sh -c 'ip link set $eth_interface address $mac_spoof'" \
 	"ExecStart = /usr/bin/sh -c 'ip link set $eth_interface up'" \
 	"ExecStart = /usr/bin/sh -c 'ip route add default via $gateway_ip dev $eth_interface'" \
 	"ExecStop = /usr/bin/sh -c 'ip addr del $static_ip/24 broadcast + dev $eth_interface'" \
@@ -237,8 +244,8 @@ printf "%s\n" \
 	"RemainAfterExit = yes" \
 	"" \
 	"[Install]" \
-	"WantedBy = multi-user.target" > /mnt/etc/systemd/system/network.service
-arch-chroot /mnt systemctl enable network.service
+	"WantedBy = multi-user.target" > /mnt/etc/systemd/system/network.service || error_exit "Failed to define systemd network service"
+arch-chroot /mnt systemctl enable network.service || error_exit "Failed to enable systemd network service"
 
 # add alias for sudo="Sudo -E"
 # add alias for bc="bc -l"
